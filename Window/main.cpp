fb683fd303a9bd4f9df68aa34453f6ba52802e0c
maficccc@gmail.com <maficccc@gmail.com>
2017-09-17 23:47:57 +0200

    Little change, added version opengl

--- Window/main.cpp
+++ Window/main.cpp
@@ -4,14 +4,18 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
+    {
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL!", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
